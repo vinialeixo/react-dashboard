da11bb8726ee0e2edc67373573fae27ae5781ea1
vinialeixo <vi_aleixodias@hotmail.com>
2022-10-28 17:00:04 -0300

    feat: make a filter dynamic

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,15 @@
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 import "./ExpenseItem.css";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandlres = () => {
-    setTitle("Title Changed!!");
-    console.log(title);
-  };
-
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandlres}>Change Title</button>
     </div>
   );
 };
